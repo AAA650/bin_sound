885c1107d404719390502a5302eaefbe418ccd00
AAA650 <W3493593104@163.com>
2025-05-02 21:52:47 +0800

    updated res folder

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <filesystem>
 #include <windows.h>
 #include <mmsystem.h>
@@ -8,43 +9,132 @@
 using namespace std;
 using namespace filesystem;
 
-int main(int argc, char* argv[])
-{
-	path input_file = "src.bin", zero_audio = "zero.wav", one_audio = "one.wav";
-	for (int i = 0; i < argc; i++) {
-		if (strcmp(argv[i], "-f") == 0)
-			input_file = argv[++i];
-		else if (strcmp(argv[i], "-z") == 0)
-			zero_audio = argv[++i];
-		else if (strcmp(argv[i], "-o") == 0)
-			one_audio = argv[++i];
+vector<path>* get_res_dir(const path& search_dir) {
+	if (!is_directory(search_dir))
+		return nullptr;
+
+	vector<path>* path_list = new vector<path>;
+
+	for (auto const& dir_it : directory_iterator(search_dir)) {
+		if (is_regular_file(dir_it.path() / "zero.wav")
+			&& is_regular_file(dir_it.path() / "one.wav")) {
+			path_list->push_back(dir_it.path());
+		}
 	}
 
-	if (!is_regular_file(input_file)) {
-		cerr << "Input is not a regular file" << endl;
-		system("pause");
+	return path_list;
+}
+
+int main(int argc, char* argv[])
+{
+	// init vars
+	path in_bin_file = "res.bin", res_path;
+	bool is_res_set = false;
+
+	// process args
+	for (int i = 0; i < argc; i++) {
+		// set in_bin_file
+		if (strcmp(argv[i], "-f") == 0)
+			in_bin_file = argv[++i];
+		// set res_dir
+		else if (strcmp(argv[i], "-p") == 0)
+		{
+			res_path = argv[++i];
+			is_res_set = true;
+		}
+	}
+
+	if (!is_res_set) {
+		// if res dir was not set in args
+		// find dirs in current_path/res
+		vector<path>* res_list = get_res_dir(current_path() / "res");
+
+		// check res_list
+		if (res_list == nullptr) {
+			cerr << "No legal directories detected" << endl;
+			return -1;
+		}
+
+		cout << "Res was not set! Choose following folders detected." << endl;
+		
+		// output dir detected
+		for (int i = 0; i < res_list->size(); i++) {
+			cout << i << ") " << res_list->at(i) << endl;
+		}
+
+		//accept users input
+		for (int input;;) {
+			// input res offset
+			cout << "Pls choose one: ";
+			cin >> input;
+
+			// check input
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore();
+				continue;
+			}
+
+			// if out_of_range or other errors occered
+			try{
+				res_path = res_list->at(input);
+			}// then redo for loop
+			catch (exception ex) {
+				continue;
+			}
+
+			break;
+		}
+
+		delete res_list;
+	}
+	else if (!is_directory(res_path)) {
+		cerr << "Res is not a directory" << endl;
 		return -1;
 	}
 
+	// set zero/one audio
+	path zero_audio = res_path / "zero.wav",
+		one_audio = res_path / "one.wav";
+
+	// check if file existing
+	if (!is_regular_file(in_bin_file)) {
+		cerr << "Input " << in_bin_file.c_str() << " is not a regular file" << endl;
+		system("pause");
+		return -1;
+	}
 	if (!is_regular_file(zero_audio)) {
 		cerr << "Zero is not a regular file" << endl;
 		system("pause");
 		return -1;
 	}
-
 	if (!is_regular_file(one_audio)) {
 		cerr << "One is not a regular file" << endl;
 		system("pause");
 		return -1;
 	}
 
-	ifstream* bin_file = new ifstream(input_file, ios::in | ios::binary);
+	// read input_bin_file
+	ifstream* bin_file = new ifstream(in_bin_file, ios::in | ios::binary);
+
+	// count down to start
+	cout << "Ready? 3... ";
+	Sleep(1000);
+	cout << "2... ";
+	Sleep(1000);
+	cout << "1... " << endl;
+	Sleep(1000);
 
 	for (int byte_data = 0; !bin_file->eof();) {
+		// process each byte
 		byte_data = bin_file->get();
+
 		for (int i = (sizeof byte_data) * 2 - 1; i >= 0; i--) {
+			// process each bit
 			int bit_data = (byte_data >> i) & 1;
 			cout << bit_data;
+
+			// play sound
 			switch (bit_data) {
 			case 0:
 				PlaySound(zero_audio.c_str(), NULL, SND_FILENAME);
@@ -57,6 +147,7 @@ int main(int argc, char* argv[])
 		cout << endl;
 	}
 
+	// free data
 	bin_file->close();
 	delete bin_file;
 
